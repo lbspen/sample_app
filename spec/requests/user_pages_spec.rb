97023200be647edbe430550ad6fcfaf7fe7fafd1
lbspen <lbspen@gmail.com>
2013-08-31 14:33:37 -0700

    Add improvements from end of chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,16 +8,15 @@ describe "User pages" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_content(user.name) }
+    it { should have_selector('h1', text: user.name) }
     it { should have_title(user.name) }
   end
 
   describe "signup page" do
     before { visit signup_path }
 
-    it { should have_content("Sign up") }
+    it { should have_selector('h1', text: "Sign up") }
     it { should have_title(full_title("Sign up")) }
-    
   end
 
   describe "signup" do
@@ -28,7 +27,19 @@ describe "User pages" do
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count) 
-      end      
+      end 
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_selector("#error_explanation", text: 'error') }
+        it { should have_selector("li", text: "Name can't be blank") }
+        it { should have_selector("li", text: "Email can't be blank") }
+        it { should have_selector("li", text: "Email is invalid") }
+        it { should have_selector("li", text: "Password is too short (minimum is 6 characters)") }
+        it { should have_selector("li", text: "Password can't be blank") }
+      end     
     end
 
     describe "with valid information" do
@@ -42,6 +53,14 @@ describe "User pages" do
       it "should create a user" do
         expect{ click_button submit }.to change(User, :count).by(1)
       end
-    end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+   end
   end
 end
